95a1524575d0d041c7451c052f7c2431f7508e05
Brunno Telma <brunnotelma@gmail.com>
2018-06-29 00:26:54 -0300

    [Add] Arduino program to read sensor and communicate through Serial

---
+++ sensors/sensors.ino
@@ -0,0 +1,56 @@
+#define LEVEL_1 9
+#define LEVEL_2 10
+#define LEVEL_3 11
+#define LEVEL_4 12
+
+void setup() {
+    pinMode(LEVEL_1, INPUT);
+    pinMode(LEVEL_2, INPUT);
+    pinMode(LEVEL_3, INPUT);
+    pinMode(LEVEL_4, INPUT);
+
+    Serial.begin(9600);
+}
+
+String readSerial;
+int levels[] = {
+    LEVEL_1,
+    LEVEL_2,
+    LEVEL_3,
+    LEVEL_4,
+};
+
+void loop() {
+    delay(100);
+
+    // Wait until there is data available
+    if (Serial.available() > 0) {
+        readSerial = Serial.readString();
+        readSerial.trim();
+
+        if (readSerial == "water_temperature") {
+            // Read water temperature and sends through Serial (RX/TX)
+            Serial.print("60");
+        }else if(readSerial == "steam_temperature"){
+            // Read steam temperature and sends through Serial (RX/TX)
+            Serial.print("75");
+        }else if(readSerial == "water_level"){
+            // Read water level and sends through Serial (RX/TX)
+            Serial.print(getWaterLevel());
+        }
+    }
+}
+
+int getWaterLevel() {
+    int water_level = 0;
+
+    for(int i = 0; i < 4; i++) {
+        delay(50);
+        
+        if(digitalRead(levels[i]) == HIGH){
+            water_level = i + 1;
+        }
+    }
+
+    return water_level;
+}
