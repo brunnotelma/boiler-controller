fa8f1b2363fca176c10c315d476f6aedb5be3764
Brunno Telma <brunnotelma@gmail.com>
2018-07-03 04:49:23 -0300

    [Add] Comments and main loop

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ relays = {
 }
 
 def setup():
+    # For each relay pin, set its GPIO to output and state = 1
     for relay in relays.itervalues():
         relay.setOutputDirection(RELAY_OFF)
 
@@ -28,16 +29,19 @@ def getWaterLevel():
     # Setup serial port
     ser = serial.Serial(port='/dev/ttyS1', baudrate=9600, timeout=2)
     waterLevel = ""
-        
+
+    # Keeps in the loop while value received is not valid
     while(not isInt(waterLevel)):
         # Requests water level from Arduino
         ser.write("water_level")
         # Read 8 byte response
         waterLevel = ser.read(8)
+        # Closes communication port
         ser.cancel_read()
 
     return int(waterLevel)
 
+# Sets relay state, based on its name
 def setSolenoidState(relayName, state):
     # int(not value) inverts a integer (0/1)
     relays['close' + relayName].setValue(int(not state))
@@ -45,28 +49,33 @@ def setSolenoidState(relayName, state):
 
 def main():
     setup()
-    relayTest(RELAY_ON)
-    relayTest(RELAY_OFF)
 
     while True:
-        # 1-sec delay on the beggining of the loop just to decrease computational cost
-        time.sleep(0.5)
+        # 100ms delay on the beggining of the loop just to decrease computational cost
+        time.sleep(0.1)
 
         waterLevel = getWaterLevel()
         print("Level: " + str(waterLevel))
 
-        if waterLevel >= 3:
-            setSolenoidState(GAS_RELAY, RELAY_ON)
-
         if waterLevel < 4:
+            setSolenoidState(STEAM_RELAY, RELAY_OFF)
             setSolenoidState(WATER_RELAY, RELAY_ON)
+
+            if waterLevel < 2 :
+                setSolenoidState(GAS_RELAY, RELAY_OFF)
+            else:
+                setSolenoidState(GAS_RELAY, RELAY_ON)
         else:
             setSolenoidState(WATER_RELAY, RELAY_OFF)
-            # if waterLevel == 5:
-            #     setSolenoidState(GAS_RELAY, RELAY_OFF)
+            if waterLevel == 5:
+                setSolenoidState(STEAM_RELAY, RELAY_ON)
+                setSolenoidState(GAS_RELAY, RELAY_OFF)
+            else:
+                setSolenoidState(GAS_RELAY, RELAY_ON)
 
     return 0
 
+# Checks if string content is an integer
 def isInt(string):
     try: 
         int(string)
@@ -74,6 +83,7 @@ def isInt(string):
     except ValueError:
         return False
 
+# Test all relays, turning them on and off based on the param `relayStatus`
 def relayTest(relayStatus):
     print("Turning relays " + ("Off" if relayStatus else "On") + "...")
 
